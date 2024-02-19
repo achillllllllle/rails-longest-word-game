f7d433f07e5f0136074b21bd757baa3bf6f3cb75
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-19 16:34:43 +0100

    longest

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @array = []
+    alphabet = ('A'..'Z').to_a
+    for i in 0..10 do
+      @array.push(alphabet.sample(1))
+    end
+  end
+
+  def score
+    check = true
+    list = params["array"].split('')
+    @reponse = params["reponse"].split('')
+    @reponse.each do |letter|
+      unless list.include?(letter)
+        @message = "Sorry but #{@reponse.join.upcase} can't be built out of #{params["array"]}"
+        check = false
+      else
+        list.delete_at(list.find_index(letter))
+      end
+    end
+    if check
+      url = "https://wagon-dictionary.herokuapp.com/#{params['reponse']}"
+      word_serialized = URI.open(url).read
+      word = JSON.parse(word_serialized)
+      unless word["found"]
+        @message = "Sorry but #{@reponse.join.upcase} does not seem to be a valid English word..."
+      else
+        @message = "Congratulations ! #{@reponse.join.upcase} is a valid English word!"
+      end
+    end
+  end
+end
